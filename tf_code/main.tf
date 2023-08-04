d68b703224a601660e39a4809b8a69817e49e3b3
Rakesh Kumar Sahu <118262896+rakeshnexturn@users.noreply.github.com>
2023-08-04 18:22:36 +0530

    Update main.tf

--- tf_code/main.tf
+++ tf_code/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "my_instance" {
-  ami                    = "ami-08581e2e50ad52e16"
+  ami                    = "ami-053b0d53c279acc90"
   vpc_security_group_ids = [aws_security_group.my_security_group.id]
   subnet_id              = aws_subnet.my_subnet.id
   count                  = 1
